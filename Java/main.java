58428ebe036d99624020121dae9738ba87de6c90
simonzv02 <simonzv2102@gmail.com>
2024-08-14 14:47:20 -0500

    rojo es gei?

--- Java/main.java
+++ Java/main.java
@@ -4,6 +4,12 @@ class simon{
         System.out.println("Hola, este es mi primer script");
         int precio = 45;
         System.out.println("precio: " + (precio + 98));
-        
+        String cadena = "Rojo es gei";
+        System.out.println(cadena);
+        Float precios = 2.55f;
+        System.out.println(precios + 35);
+        boolean rojoesgei = true;
+        System.out.println(rojoesgei);
+
     }
 }
