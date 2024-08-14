4033430fd70f00f016a6ffe7dedb73b5c690e1d7
simonzv02 <simonzv2102@gmail.com>
2024-08-14 10:03:32 -0500

    primer hola mundo

---
+++ Java/main.java
@@ -0,0 +1,9 @@
+package Java;
+class simon{
+    public static void main(String[] args) {
+        System.out.println("Hola, este es mi primer script");
+        int precio = 45;
+        System.out.println("precio: " + (precio + 98));
+        
+    }
+}
