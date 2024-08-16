0b8036a9edc0743a2d8a31350c3d4489fbb2e589
simonzv02 <simonzv2102@gmail.com>
2024-08-16 08:37:14 -0500

    personajes de mario

---
+++ Java/ciclofor.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class ciclofor {
+    public static void main(String[] args) {
+    String mariobroscharacters = "luigui";
+    char[] listLetrasmariobroscharacters = mariobroscharacters.toCharArray();
+
+
+    for     (int i = 0; i < 7 ; i++) {
+        String letra = Character.toString(listLetrasmariobroscharacters[i]);
+
+
+    }
+    }
+}
