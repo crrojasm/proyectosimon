e014b6a6997001f5a3e2336afc13b08bf36082c4
simonzv02 <simonzv2102@gmail.com>
2024-08-20 12:47:29 -0500

    tarea 1

--- Java/ciclofor.java
+++ Java/ciclofor.java
@@ -7,22 +7,10 @@ public class ciclofor {
 
 
     for (int i = 0; i <  mariobroscharacters.length ; i++) {
-      //System.out.println((i + 1) + ":" + mariobroscharacters[i]);
+      System.out.println("iteracion " + i);
         
-        String Inicial = mariobroscharacters[i];
-        
-        if(Inicial.equals("Luigui")){
-            System.out.println("L");
-        } else if (Inicial.equals("Mario")){
-            System.out.println("M");
-        } else if (Inicial.equals("Peach")){
-            System.out.println("P");
-        } else if (Inicial.equals("Toad")){
-            System.out.println("T");
-        } else if (Inicial.equals("Mario")){
-            System.out.println("M");
-        }    else if (Inicial.equals("Bowser")){
-                System.out.println("B");    
+        if (mariobroscharacters[i].equals("Peach")){
+            break;
         }
 
 
---
+++ Java/numeros_impares.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class numeros_impares {
+    public static void main(String[] args) {
+        for (int i = 3; i <= 17; i +=2) {
+            System.out.println("Número " + i);
+
+        }
+    }
+    
+}
